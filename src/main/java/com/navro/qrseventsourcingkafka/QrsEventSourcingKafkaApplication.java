39396cb9292dc52025762725c3d2f0d9efd275b7
Tomasz Nawrocki <tomasz.nawrocki@codeshine.com>
2025-02-13 16:33:31 +0100

    initial commit

---
+++ src/main/java/com/navro/qrseventsourcingkafka/QrsEventSourcingKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.navro.qrseventsourcingkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrsEventSourcingKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrsEventSourcingKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navro/qrseventsourcingkafka/QrsEventSourcingKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navro.qrseventsourcingkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrsEventSourcingKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
